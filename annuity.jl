d6d215bcb08071f66c08bc3e4a16809f70edec98
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-08-04 12:13:28 +0300

    calculations

--- annuity.jl
+++ annuity.jl
@@ -1,8 +1,15 @@
-r = (1 + 0.06)^(1/12) - 1
-n = 20 * 12
+y = []
+r_ys = 0.00:0.001:0.06
+for r_y in r_ys
+    r = (1 + r_y)^(1/12) - 1
+    n = 20 * 12
 
-k = sum((1+r)^(i-1) for i in 1:n)
+    k = sum((1+r)^(i-1) for i in 1:n)
+    m = k / (1 + k * r)
 
-ratio = k / (1 + k * r)
+    push!(y, m / n)
+end
 
-println(ratio)
+using Plots
+plt = plot(r_ys, y)
+savefig(plt, "ratios.svg")
