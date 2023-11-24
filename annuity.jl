0d948477514867f2f51943e33fc8503ea13dbd02
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-11-24 15:27:10 +0200

    updates

--- annuity.jl
+++ annuity.jl
@@ -1,15 +1,21 @@
-y = []
-r_ys = 0.00:0.001:0.06
-for r_y in r_ys
-    r = (1 + r_y)^(1/12) - 1
-    n = 20 * 12
+using Plots
 
-    k = sum((1+r)^(i-1) for i in 1:n)
-    m = k / (1 + k * r)
+r_ys = 0.00:0.001:0.10
+n_ys = 5:5:25
+plt = plot(; xticks=0:0.01:0.1)
+for n_y in n_ys
+    y = []
+    for r_y in r_ys
+        r = (1 + r_y)^(1/12) - 1
+        n = n_y * 12
 
-    push!(y, m / n)
+        k = sum((1+r)^(i-1) for i in 1:n)
+        m = k / (1 + k * r)
+
+        #push!(y, m / n)
+        push!(y, n / m)
+    end
+    plot!(plt, r_ys, y; label="n=$(n_y)")
 end
 
-using Plots
-plt = plot(r_ys, y)
-savefig(plt, "ratios.svg")
+savefig(plt, "ratios_inverse.svg")
